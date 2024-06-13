decda40c70b2f99c1325045674c179b024722691
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-13 23:51:53 +0530

    Controller

---
+++ src/main/java/com/SpringBootAPI/Teachers_Attendance/Controller/TeacherController.java
@@ -0,0 +1,38 @@
+package com.SpringBootAPI.Teachers_Attendance.Controller;
+
+import com.SpringBootAPI.Teachers_Attendance.Entity.Teachers;
+import com.SpringBootAPI.Teachers_Attendance.Repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+
+@RestController
+public class TeacherController {
+
+    @Autowired
+    TeacherRepository trepo;
+
+    @GetMapping("/test")
+    public String test(){
+        return "This Is A Test Run";
+    }
+
+    @PostMapping("/save")
+    public Mono<Teachers> save(@RequestBody Teachers teachers){
+        return trepo.save(teachers);
+    }
+
+    @GetMapping(value = "/all",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Teachers> findAll(){
+        Flux<Teachers> flux=trepo.findAll().log();
+        return flux;
+    }
+
+}
