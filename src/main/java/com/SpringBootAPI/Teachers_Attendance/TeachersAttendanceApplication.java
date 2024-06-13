43c3e86ac2a74245782c6ab5a2843a3df71ec27a
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-13 23:17:27 +0530

    Deployment

---
+++ src/main/java/com/SpringBootAPI/Teachers_Attendance/TeachersAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootAPI.Teachers_Attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachersAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachersAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootAPI/Teachers_Attendance/TeachersAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootAPI.Teachers_Attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachersAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
