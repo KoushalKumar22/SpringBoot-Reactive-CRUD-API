9a6e382bc18483e93c72a371d7a12f750b7f9269
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-14 22:38:57 +0530

    update and delete

--- src/main/java/com/SpringBootAPI/Teachers_Attendance/Controller/TeacherController.java
+++ src/main/java/com/SpringBootAPI/Teachers_Attendance/Controller/TeacherController.java
@@ -3,13 +3,13 @@ package com.SpringBootAPI.Teachers_Attendance.Controller;
 import com.SpringBootAPI.Teachers_Attendance.Entity.Teachers;
 import com.SpringBootAPI.Teachers_Attendance.Repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Target;
 
 
 @RestController
@@ -32,6 +32,29 @@ public class TeacherController {
     public Flux<Teachers> findAll(){
         Flux<Teachers> flux=trepo.findAll().log();
         return flux;
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<String> update(@PathVariable int id, @RequestBody Teachers updatedTeacher) {
+        return trepo.findById(id)
+                .flatMap(existingTeacher -> {
+                    existingTeacher.setName(updatedTeacher.getName());
+                    existingTeacher.setPrimarySub(updatedTeacher.getPrimarySub());
+                    existingTeacher.setSecondarySub(updatedTeacher.getSecondarySub());
+                    existingTeacher.setEntryTime(updatedTeacher.getEntryTime());
+                    return trepo.save(existingTeacher);
+                })
+                .map(savedTeacher -> "Teacher updated successfully with ID: " + savedTeacher.getId())
+                .defaultIfEmpty("Teacher not found with ID: " + id);
+    }
+
+    @DeleteMapping("/del/{id}")
+    public Mono<String> deleteById(@PathVariable int id){
+        return trepo.findById(id).flatMap(existingTeachers ->
+                trepo.delete(existingTeachers)
+                        .then(Mono.just("Data Deleted by Id: "+id))
+        )
+                .defaultIfEmpty("Teacher Data Not Found With Id: "+id);
     }
 
 }
